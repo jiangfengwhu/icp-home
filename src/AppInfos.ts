d95a578f24cc2d02fa3e070a2d10b487b6e8a5f8
fengjiang <jiangfengwhu2011@gmail.com>
2025-04-06 09:30:57 +0800

    feat: 更新app商店地址

--- src/AppInfos.ts
+++ src/AppInfos.ts
@@ -6,7 +6,7 @@ export const apps = [
     nameKey: "appInfo.ledger",
     descriptionKey: "appInfo.ledgerDesc",
     platform: "IOS",
-    storeUrl: "https://example.com/app1",
+    storeUrl: "https://apps.apple.com/us/app/%E6%99%BA%E8%B4%A6/id6743987528",
     icon: NeoLedger,
   },
   {
